a01ca0bc2d611477b85e762812200d5722a63c16
Pavel Larin <larinpavelfirst@mail.ru>
2023-09-05 12:11:08 -0300

    A new project for controlling the intensity of bicycle lights to economize battery life! :)

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello! We are writing a program that is going to control the intensity of bicycle lights to economize battery life! :)")
+}
