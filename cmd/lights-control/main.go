62242819122ee17d172b24e41d80a1d412ec7696
Pavel Larin <larinpavelfirst@mail.ru>
2023-09-09 15:21:10 -0300

    Added the Outputter interface and the ConsoleOutputter type

--- cmd/lights-control/main.go
+++ cmd/lights-control/main.go
@@ -5,12 +5,25 @@ import (
 	"golang-bicycle-lights/cmd/lights-control/pkg/light"
 )
 
-func main() {
+type Outputter interface {
+	// Display takes a string and displays it to the user
+	Display(string)
+}
 
-	defer func() { // Declare a deferred function that will run after main returtns
+// ConsoleOutputter is a type that implements the Outputter interface using fmt.Println
+type ConsoleOutputter struct{}
+
+// Display takes a string and displays it to the standard output using fmt.Println
+func (c *ConsoleOutputter) Display(s string) {
+	fmt.Println(s)
+}
+
+func run(output Outputter) {
+
+	defer func() {                    // Declare a deferred function that will run after main returtns
 		if r := recover(); r != nil { // check if there was a panic
 			fmt.Println("An error occurred while reading input. Please try again.", r) // Print a friendly error message
-			main()                                                                     // Call main again to restart the program
+			main()                                                                     // Call main again to restart the program                                                                     // Call main again to restart the program
 		}
 	}()
 
@@ -23,9 +36,18 @@ func main() {
 		if envLight == -1 {
 			fmt.Println("Thanks for using our program! :) Exiting.")
 			running = false
+		} else {
+			output.Display(light.Check(envLight)) // Display the result of the light.Check function
+			output.Display("🚲")                   // Display the bicycle emoji
 		}
-
-		fmt.Println(light.Check(envLight))
-		fmt.Println("🚲")
 	}
+
+}
+
+func main() {
+	// Create an outputter instance
+	output := &ConsoleOutputter{}
+
+	// Call the run function with the outputter instance
+	run(output)
 }
